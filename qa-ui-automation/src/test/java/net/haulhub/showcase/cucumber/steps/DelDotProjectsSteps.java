ccb19776b98e7c53c1cae62ada96dfbd575011ca
Kaveendra Vithanawasam <kaveendralk@gmail.com>
2021-03-09 01:06:43 +0530

    DotSlip Inspector tests

--- qa-ui-automation/src/test/java/net/haulhub/showcase/cucumber/steps/DelDotProjectsSteps.java
+++ qa-ui-automation/src/test/java/net/haulhub/showcase/cucumber/steps/DelDotProjectsSteps.java
@@ -19,34 +19,54 @@ public class DelDotProjectsSteps {
 	@Steps
 	SerenityDelDotProjectsSteps serenitydeldotprojectssteps;
 
-	@Given("I validate assigned project is displayed in projects page(.*)")
+	@Then("I validate assigned project is displayed in projects page(.*)")
 	public void validateAssignProjects(String projName) throws Exception {
 		serenitydeldotprojectssteps.validateAssignProjects(projName);
 	}
 	
-	@Given("I validate archived project is displayed in archived projects(.*)")
+	@Then("I validate archived project is displayed in archived projects(.*)")
 	public void validateArchiveProjects(String projectName) throws Exception {
 		serenitydeldotprojectssteps.validateArchiveProjects(projectName);
 	}
 	
-	@Given("I search by (.*) and validate staff (.*) and (.*) in projects")
+	@Then("I search by (.*) and validate staff (.*) and (.*) in projects")
 	public void validateStaffSearch(String staffName, String staffRole, String staffPhone) throws Exception {
 		serenitydeldotprojectssteps.validateStaffSearchF(staffName, staffRole, staffPhone);
 	}
 	
-	@Given("I validate completed project functionality in projects(.*)")
+	@Then("I validate completed project functionality in projects(.*)")
 	public void validateCompletedProjects(String projectName) throws Exception {
 		serenitydeldotprojectssteps.validateCompletedProjects(projectName);
 	}
 	
-	@Given("I validate displayed data in slip card")
+	@Then("I validate displayed data in slip card")
 	public void validateDataInSlipCard() throws Exception {
 		serenitydeldotprojectssteps.validateDataInSlipCard();
 	}
 	
-	@Given("I filter by ticket number in slip card(.*)")
+	@Then("I filter by ticket number in slip card(.*)")
 	public void filterByTicketNo(String ticketNo) throws Exception {
 		serenitydeldotprojectssteps.filterByTicketNo(ticketNo);
 	}
-
+	
+	@Then("I filter by contractor in slip card(.*)")
+	public void filterByContractor(String contracter) throws Exception {
+		serenitydeldotprojectssteps.filterByContractor(contracter);
+	}
+	
+	@Then("I filter by supplier in slip card(.*)")
+	public void filterBySupplier(String supplier) throws Exception {
+		serenitydeldotprojectssteps.filterBySupplier(supplier);
+	}
+	
+	@Then("I filter by material in slip card(.*)")
+	public void filterByMateial(String material) throws Exception {
+		serenitydeldotprojectssteps.filterByMateial(material);
+	}
+	
+	@Then("I logout from DelDOT")
+	public void validateLogoutFunctionality() throws Exception {
+		serenitydeldotprojectssteps.validateLogoutFunctionality();
+	}
+	
 }
